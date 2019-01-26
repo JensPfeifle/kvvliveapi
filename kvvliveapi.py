86ad64cf4f3e4bb7af5807927f72851249ba7890
jens <jp@jens.tech>
2019-01-26 13:43:20 +0100

    autoPEP8, remove duplicate import

--- kvvliveapi.py
+++ kvvliveapi.py
@@ -3,18 +3,18 @@
 
 try:
     import urllib.request as _urllib
-    from urllib.parse import quote_plus,urlencode
+    from urllib.parse import quote_plus, urlencode
 except ImportError:
     import urllib2 as _urllib
-    from urllib import quote_plus,urlencode
-from datetime import datetime,timedelta
+    from urllib import quote_plus, urlencode
+from datetime import datetime, timedelta
 import re
 import json
-import re
 import sys
 
 API_KEY = "377d840e54b59adbe53608ba1aad70e8"
 API_BASE = "https://live.kvv.de/webapp/"
+
 
 class Stop:
     def __init__(self, name, stop_id, lat, lon):
@@ -38,7 +38,7 @@ class Departure:
         self.route = route
         self.destination = destination
         self.direction = direction
-        self.timestr = time #TODO: to timestamp?
+        self.timestr = time  # TODO: to timestamp?
         self.lowfloor = lowfloor
         self.realtime = realtime
         self.traction = traction
@@ -107,7 +107,8 @@ class Departure:
         return Departure(json["route"], json["destination"], json["direction"], time, json["lowfloor"], json["realtime"], json["traction"], json["stopPosition"])
 
     def pretty_format(self, alwaysrelative=False):
-        return self.pretty_time(alwaysrelative) + self.route + " " + self.destination #TODO check
+        # TODO check
+        return self.pretty_time(alwaysrelative) + self.route + " " + self.destination
 
     def pretty_time(self, alwaysrelative=False):
         if alwaysrelative and self.timestr != "sofort":
@@ -116,21 +117,21 @@ class Departure:
         else:
             timestr = self.timestr
         return timestr + ("  " if self.realtime else "* ") + (
-        " " if timestr != "sofort" else "")
+            " " if timestr != "sofort" else "")
 
     def __repr__(self):
         return "Departure(route={}, destination={}, direction={}, time={})"\
             .format(self.route, self.destination, self.direction, self.time)
 
 
-def _query(path, params = {}):
+def _query(path, params={}):
     params["key"] = API_KEY
     url = API_BASE + path + "?" + urlencode(params)
     req = _urllib.Request(url)
 
-    #try:
+    # try:
     handle = _urllib.urlopen(req)
-    #except IOError as e:
+    # except IOError as e:
     #    if hasattr(e, "code"):
     #        if e.code != 403:
     #            print("We got another error")
@@ -142,6 +143,7 @@ def _query(path, params = {}):
 
     return json.loads(handle.read().decode("utf8"))
 
+
 def _search(query):
     json = _query(query)
     stops = []
@@ -150,11 +152,13 @@ def _search(query):
             stops.append(Stop.from_json(stop))
     return stops
 
+
 def search_by_name(name):
     """ Search for stops by name
         returns a list of Stop objects
     """
     return _search("stops/byname/" + quote_plus(name))
+
 
 def search_by_latlon(lat, lon):
     """ Search for stops by latitude and longitude
@@ -162,14 +166,16 @@ def search_by_latlon(lat, lon):
     """
     return _search("stops/bylatlon/" + lat + "/" + lon)
 
+
 def search_by_stop_id(stop_id):
     """ Search for a stop by its stop_id
         returns a list that should contain only one stop
     """
     return [Stop.from_json(_query("stops/bystop/" + stop_id))]
 
+
 def _get_departures(query, max_info=10):
-    json = _query(query, {"maxInfos" : str(max_info)})
+    json = _query(query, {"maxInfos": str(max_info)})
     departures = []
     if json:
         for dep in json["departures"]:
@@ -183,11 +189,13 @@ def get_departures(stop_id, max_info=10):
     """
     return _get_departures("departures/bystop/" + stop_id, max_info)
 
+
 def get_departures_by_route(stop_id, route, max_info=10):
     """ Return a list of Departure objects for a given stop stop_id and route
         optionally set the maximum number of entries 
     """
     return _get_departures("departures/byroute/" + route + "/" + stop_id, max_info)
+
 
 def _errorstring(e):
     if hasattr(e, "code"):
@@ -216,6 +224,7 @@ if __name__ == "__main__":
             for dep in get_departures_by_route(sys.argv[2], sys.argv[3]):
                 print(dep.pretty_format())
         else:
-            print("No such command. Try \"search <name>/<stop_id>/<lat> <lon>\" or \"departures <stop stop_id> [<route>]\"")
+            print(
+                "No such command. Try \"search <name>/<stop_id>/<lat> <lon>\" or \"departures <stop stop_id> [<route>]\"")
     except IOError as e:
-       sys.stderr.write("{}\n".format(_errorstring(e)));
+        sys.stderr.write("{}\n".format(_errorstring(e)))
