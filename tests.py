383c13b64022a2355952f74326e4075c743ff75f
jenspfeifle <jens@pfeifle.tech>
2019-03-08 21:38:57 +0100

    update tests

--- tests.py
+++ tests.py
@@ -1,5 +1,8 @@
+print("Testing imports.")
 import kvvliveapi
 import datetime
 from bottle import Bottle, route, run, debug, template, static_file, request
 
-print("(Useless) tests complete.")
+
+
+print("Tests complete.")
